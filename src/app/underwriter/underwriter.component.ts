f28b3ac88f26c8e7e6f72f2bbd14219947aee6ac
Piyush K. Das <piyush.k.das25@gmail.com>
2024-12-07 03:46:31 +0530

    underwriter dashboard

---
+++ src/app/underwriter/underwriter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-underwriter',
+  standalone: false,
+  
+  templateUrl: './underwriter.component.html',
+  styleUrl: './underwriter.component.css'
+})
+export class UnderwriterComponent {
+
+}
